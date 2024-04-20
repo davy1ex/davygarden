b195e2d6d13e492fb79e0c49acdd97910cb410e8
davy1ex <davy1ex@yandex.ru>
2024-04-20 23:35:21 +0400

    added styles

--- public/post.php
+++ public/post.php
@@ -2,9 +2,20 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); 
+?>
 
-require_once '../app/Posts.php';
+<head>
+    <link rel="stylesheet" href="css/posts.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+
+</head>
+<?php
 require_once 'header.php';
+require_once '../app/Posts.php';
+
+
+
 
 $Posts = new Posts();
 $postName = $_GET['name'] ?? 'default.md';
@@ -20,3 +31,6 @@ if ($post) {
 else {
     echo "404 :с";
 }
+
+?>
+<script>hljs.highlightAll();</script>
